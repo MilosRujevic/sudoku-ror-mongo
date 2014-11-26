c0137b887cf667299bbf063fcbeed0f672181dda
itmilos <itmilos@outlook.com>
2014-11-26 14:02:48 +0100

    Add part I

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,20 +6,37 @@ class Game
   field :sudoku
 
   def generate_sudoku
+    #  Matrix.build(3, 3) {|row, col| rand(2) } 
     res = []
-    res[0] = [1,2,3,4,5,nil,7,8,9]
-    res[1] = [1,2,3,4,5,6,7,8,9]
-    res[2] = [1,2,3,4,5,6,7,8,9]
-    res[3] = [1,2,3,4,5,6,7,8,9]
-    res[4] = [1,2,3,4,5,6,7,8,9]
-    res[5] = [1,2,3,4,5,6,7,8,9]
-    res[6] = [1,2,3,4,5,6,7,8,9]
-    res[7] = [1,2,3,4,5,6,7,8,9]
-    res[8] = [1,2,3,4,5,6,7,8,9]
+    res[0] = rand(0 .. 9)
+    res[1] = rand(0 .. 9)
+    res[2] = rand(0 .. 9)
+    res[3] = rand(0 .. 9)
+    res[4] = rand(0 .. 9)
+    res[5] = rand(0 .. 9)
+    res[6] = rand(0 .. 9)
+    res[7] = rand(0 .. 9)
+    res[8] = rand(0 .. 9)
     self.sudoku = res
     self.save
     res
   end
+
+  def self.matrix
+    mat = Matrix[[1,2], [3,4]]
+  end
+
+  def matrix2
+    Matrix[[1,2], [3,4]]
+  end
+
+  def generate_sudoku_matrix
+    m1 = Matrix[[1,2], [3,4]]
+    self.sudoku = m1
+    self.salve
+    m1
+  end
+
 
   belongs_to :user
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-    root 'application#index'
+    
 
-    # mount Sudoku::API => '/api'
+    mount Sudoku::API => '/api'
 
     get "*path.html" => "application#index", :layout => 0
     get "*path" => "application#index"
 
+
+    root 'application#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
